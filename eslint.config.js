575fc7358a4cd3c3256468e3bd4e7e6dd9c10aa0
Erfan <mrenmityerf@gmail.com>
2024-09-25 19:52:18 +0330

    model configure and navigation done

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,7 @@ export default [
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
--- src/assets/components/ModelView.jsx
+++ src/assets/components/ModelView.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { OrbitControls, View } from "@react-three/drei";
+import React, { Suspense } from "react";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import * as THREE from "three";
+import Loader from "./Loader";
 
-const ModelView = () => {
-  return <div>ModelView</div>;
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${
+        index === 2 ? "right-[-100%]" : ""
+      } `}
+    >
+      <ambientLight intensity={0.3}></ambientLight>
+      <perspectiveCamera makeDefault position={[0, 0, 4]}></perspectiveCamera>
+      <Lights></Lights>
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      ></OrbitControls>
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large' `}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader></Loader>}> </Suspense>
+      </group>
+      <IPhone
+        scale={index === 1 ? [20, 20, 20] : [25, 25, 25]}
+        item={item}
+        size={size}
+      ></IPhone>
+    </View>
+  );
 };
 
 export default ModelView;
