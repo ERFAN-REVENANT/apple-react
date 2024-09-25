bd086ec35fde05b49c263b238224a433d0b80d63
Erfan <mrenmityerf@gmail.com>
2024-09-25 17:35:43 +0330

    model button and size control done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import NavBar from './assets/components/NavBar'
-import Highlights from './assets/components/Highlights'
-import Hero from './assets/components/Hero'
-import "../public/tailwind.css"
+import React from "react";
+import NavBar from "./assets/components/NavBar";
+import Highlights from "./assets/components/Highlights";
+import Hero from "./assets/components/Hero";
+import "../public/tailwind.css";
+import Model from "./assets/components/Model";
 
 const App = () => {
   return (
-    <main className='bg-black'>
+    <main className="bg-black">
       <NavBar></NavBar>
       <Hero></Hero>
       <Highlights></Highlights>
+      <Model></Model>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
